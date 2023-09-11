c6893f73b32ff4b66f33a5af11dce85a952f49ab
pajicadvance <pavle.pajic23@gmail.com>
2023-09-11 17:05:01 +0200

    Add config trigger to dedicated server

---
+++ src/main/java/me/pajic/enchantmentdisabler/mixin/MinecraftServerMixin.java
@@ -0,0 +1,17 @@
+package me.pajic.enchantmentdisabler.mixin;
+
+import me.pajic.enchantmentdisabler.config.Config;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+
+    @Inject(method = "loadWorld", at = @At("HEAD"))
+    private void preLoad(CallbackInfo ci) {
+        new Config();
+    }
+}
