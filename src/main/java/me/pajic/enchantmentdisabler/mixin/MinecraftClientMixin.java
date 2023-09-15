864ee8b7b6f90c1517abbef3c1997bda0768251c
pajicadvance <pavle.pajic23@gmail.com>
2023-09-15 19:19:38 +0200

    Change client mixin to reload the config on integrated server start

--- src/main/java/me/pajic/enchantmentdisabler/mixin/MinecraftClientMixin.java
+++ src/main/java/me/pajic/enchantmentdisabler/mixin/MinecraftClientMixin.java
@@ -2,9 +2,9 @@ package me.pajic.enchantmentdisabler.mixin;
 
 import me.pajic.enchantmentdisabler.config.Config;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.RunArgs;
-import net.minecraft.client.realms.RealmsClient;
-import net.minecraft.resource.ResourceReload;
+import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.server.SaveLoader;
+import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
-    @Inject(method = "onInitFinished", at = @At("TAIL"))
-    private void postInit(RealmsClient realms, ResourceReload reload, RunArgs.QuickPlay quickPlay, CallbackInfo ci) {
+    @Inject(method = "startIntegratedServer", at = @At("HEAD"))
+    private void preStart(String levelName, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, boolean newWorld, CallbackInfo ci) {
         new Config();
     }
 }
