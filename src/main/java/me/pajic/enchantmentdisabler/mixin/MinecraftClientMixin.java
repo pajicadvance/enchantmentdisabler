b3ee28983451838dc12238ee3ca953a27d36c441
pajicadvance <pavle.pajic23@gmail.com>
2023-09-15 20:11:06 +0200

    1.18.2

--- src/main/java/me/pajic/enchantmentdisabler/mixin/MinecraftClientMixin.java
+++ src/main/java/me/pajic/enchantmentdisabler/mixin/MinecraftClientMixin.java
@@ -2,7 +2,6 @@ package me.pajic.enchantmentdisabler.mixin;
 
 import me.pajic.enchantmentdisabler.config.Config;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.SaveLoader;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,11 +9,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.function.Function;
+
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
-    @Inject(method = "startIntegratedServer", at = @At("HEAD"))
-    private void preStart(String levelName, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, CallbackInfo ci) {
+    @Inject(method = "startIntegratedServer(Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Function;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V", at = @At("HEAD"))
+    private void preStart(String worldName, Function<LevelStorage.Session, SaveLoader.DataPackSettingsSupplier> dataPackSettingsSupplierGetter, Function<LevelStorage.Session, SaveLoader.SavePropertiesSupplier> savePropertiesSupplierGetter, boolean safeMode, MinecraftClient.WorldLoadAction worldLoadAction, CallbackInfo ci) {
+        System.out.println("Config trigger");
         new Config();
     }
 }
