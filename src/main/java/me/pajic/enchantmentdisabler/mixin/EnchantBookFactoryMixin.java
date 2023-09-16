184bb6e80fdd0aece6a861cbab4d34cbf51438fd
Yao Chung Hu <30311066+flashyreese@users.noreply.github.com>
2023-09-16 22:55:33 +0200

    Code improvements (ty FlashyReese)
    Signed-off-by: pajicadvance <pavle.pajic23@gmail.com>

--- src/main/java/me/pajic/enchantmentdisabler/mixin/EnchantBookFactoryMixin.java
+++ src/main/java/me/pajic/enchantmentdisabler/mixin/EnchantBookFactoryMixin.java
@@ -3,19 +3,26 @@ package me.pajic.enchantmentdisabler.mixin;
 import me.pajic.enchantmentdisabler.util.EnchantmentUtil;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.village.TradeOffers;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Arrays;
 import java.util.List;
 
-@Mixin(TradeOffers.EnchantBookFactory.class)
+@Mixin(value = TradeOffers.EnchantBookFactory.class , priority = 1500) // Just in case some other mod tries to modify this class
 public abstract class EnchantBookFactoryMixin {
 
-    @ModifyArg(method = "<init>(III[Lnet/minecraft/enchantment/Enchantment;)V", at = @At(value = "INVOKE", target = "Ljava/util/Arrays;asList([Ljava/lang/Object;)Ljava/util/List;"))
-    private <T> T[] modifyArg(T[] list) {
-        List<Enchantment> possibleEnchantments = (List<Enchantment>) Arrays.asList(list);
-        return (T[]) EnchantmentUtil.removeEnchantmentsFromList(possibleEnchantments).toArray();
+    @Shadow
+    @Mutable
+    @Final
+    private List<Enchantment> possibleEnchantments;
+
+    @Inject(method = "<init>(III[Lnet/minecraft/enchantment/Enchantment;)V", at = @At(value = "TAIL"))
+    private void modifyArg(int experience, int minLevel, int maxLevel, Enchantment[] possibleEnchantments, CallbackInfo ci) {
+        this.possibleEnchantments = EnchantmentUtil.removeEnchantmentsFromList(List.of(possibleEnchantments));
     }
 }
